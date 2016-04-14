c7fc88eb354bdac0f8da07244e081d75b380ac94
kyubuns <kyubuns@gmail.com>
2016-04-14 19:23:01 +0900

    Inspectorから射影変換パラメーターを与えられるところまで

---
+++ Assets/ChocolateBar/Scripts/DebugCamera.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace ChocolateBar
+{
+    [ExecuteInEditMode]
+    public class DebugCamera : MonoBehaviour
+    {
+        [SerializeField] RenderTexture targetTexture;
+        
+        public void OnRenderImage(RenderTexture source, RenderTexture destination)
+        {
+            Graphics.Blit(targetTexture, destination);
+        }
+    }
+}
---
+++ Assets/ChocolateBar/Scripts/ProjectionCamera.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+namespace ChocolateBar
+{
+    [ExecuteInEditMode]
+    public class ProjectionCamera : MonoBehaviour
+    {
+        [SerializeField] RenderTexture targetTexture;
+        [SerializeField] Material material;
+        public Vector2 LeftTop = new Vector2(0.0f, 1.0f);
+        public Vector2 LeftBottom = new Vector2(0.0f, 0.0f);
+        public Vector2 RightBottom = new Vector2(1.0f, 0.0f);
+        public Vector2 RightTop = new Vector2(1.0f, 1.0f);
+        
+        public void OnPostRender()
+        {
+            material.mainTexture = targetTexture;
+            material.SetPass(0);
+            
+            GL.PushMatrix();
+            GL.LoadOrtho();
+            GL.Begin(GL.TRIANGLE_STRIP);
+            
+            GL.TexCoord2(0.0f, 0.0f);
+            GL.Vertex3(LeftBottom.x, LeftBottom.y, 0.0f);
+            
+            GL.TexCoord2(0.0f, 1.0f);
+            GL.Vertex3(LeftTop.x, LeftTop.y, 0.0f);
+            
+            GL.TexCoord2(1.0f, 0.0f);
+            GL.Vertex3(RightBottom.x, RightBottom.y, 0.0f);
+            
+            GL.TexCoord2(1.0f, 1.0f);
+            GL.Vertex3(RightTop.x, RightTop.y, 0.0f);
+            
+            GL.End();
+            GL.PopMatrix();
+        }
+    }
+}
